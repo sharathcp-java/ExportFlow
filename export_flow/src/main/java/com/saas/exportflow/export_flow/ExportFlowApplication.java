2ba9baf4007a8bfca00debe2144a243bca499eb4
sharathcp-java <62584109+sharathcp-java@users.noreply.github.com>
2025-10-08 01:55:07 +0530

    Initial commit

---
+++ export_flow/src/main/java/com/saas/exportflow/export_flow/ExportFlowApplication.java
@@ -0,0 +1,13 @@
+package com.saas.exportflow.export_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportFlowApplication.class, args);
+	}
+
+}
---
+++ export_flow/src/test/java/com/saas/exportflow/export_flow/ExportFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.exportflow.export_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
